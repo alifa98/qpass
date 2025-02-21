efd504274324ce83e2385a2690d82af881f6241b
Ali Faraji <31096694+alifa98@users.noreply.github.com>
2025-02-20 23:02:20 -0500

    Implement placeholder functions for encrypting and decrypting passwords to test that the cli is okay and runnable

--- src/crypto.rs
+++ src/crypto.rs
@@ -4,28 +4,32 @@ use ring::{aead, error};
 use std::str;
 
 pub fn encrypt(password: &str, key: &[u8]) -> Result<Vec<u8>, error::Unspecified> {
-    let rng = SystemRandom::new();
-    let mut nonce = [0u8; NONCE_LEN];
-    rng.fill(&mut nonce)?;
+    // // Convert password to bytes
+    // let password_bytes: &[u8] = password.as_bytes();
 
-    let unbound_key = UnboundKey::new(&AES_256_GCM, key)?;
-    let mut key = LessSafeKey::new(unbound_key);
+    // // Generate a random nonce
+    // let rng = SystemRandom::new();
+    // let mut nonce = [0u8; NONCE_LEN];
+    // rng.fill(&mut nonce)?;
 
-    let mut password_bytes = password.as_bytes().to_vec();
-    key.seal_in_place_append_tag(aead::Aad::empty(), &mut password_bytes)?;
+    
 
-    let mut encrypted_data = nonce.to_vec();
-    encrypted_data.extend(password_bytes);
-    Ok(encrypted_data)
+    // let mut encrypted_data = nonce.to_vec();
+    // encrypted_data.extend(password_bytes);
+    // Ok(encrypted_data)
+    print!("Encrypting password: {}\n", password);
+    Ok(Vec::new())
 }
 
 pub fn decrypt(encrypted_data: &[u8], key: &[u8]) -> Result<String, error::Unspecified> {
-    let unbound_key = UnboundKey::new(&AES_256_GCM, key)?;
-    let mut key = LessSafeKey::new(unbound_key);
+    // let unbound_key = UnboundKey::new(&AES_256_GCM, key)?;
+    // let mut key = LessSafeKey::new(unbound_key);
 
-    let nonce = Nonce::try_assume_unique_for_key(&encrypted_data[..NONCE_LEN])?;
-    let mut data = encrypted_data[NONCE_LEN..].to_vec();
+    // let nonce = Nonce::try_assume_unique_for_key(&encrypted_data[..NONCE_LEN])?;
+    // let mut data = encrypted_data[NONCE_LEN..].to_vec();
 
-    key.open_in_place(aead::Aad::empty(), &mut data)?;
-    Ok(String::from_utf8_lossy(&data).to_string())
+    // key.open_in_place(aead::Aad::empty(), &mut data)?;
+    // Ok(String::from_utf8_lossy(&data).to_string())
+    print!("Decrypting password\n");
+    Ok(String::new())
 }
